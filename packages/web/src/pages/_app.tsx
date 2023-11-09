6e3ff8c3f21fb71559cd39ab5d7bbffe57e15329
nitesik <neeteshchhetri69@gmail.com>
2023-11-09 14:21:22 +0600

    fixes

--- packages/web/src/pages/_app.tsx
+++ packages/web/src/pages/_app.tsx
@@ -3,6 +3,11 @@ import type { AppProps } from "next/app";
 import { client } from "@/graphql";
 import { ApolloProvider } from "@apollo/client";
 
+//@ts-ignore
+global.performance = global.performance || {
+  now: () => new Date().getTime(),
+};
+
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <ApolloProvider client={client}>
